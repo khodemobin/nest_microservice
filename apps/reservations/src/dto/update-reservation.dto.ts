be9f8aa5a9d9409c7369f6b1c15fb2049eb17b79
Mobin Yazdanparast <mobinyazdanparast9@gmail.com>
2023-08-19 17:30:08 +0330

    add validation to dtos

--- apps/reservations/src/dto/update-reservation.dto.ts
+++ apps/reservations/src/dto/update-reservation.dto.ts
@@ -1,4 +1,19 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateReservationDto } from './create-reservation.dto';
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateReservationDto extends PartialType(CreateReservationDto) {}
+export class UpdateReservationDto extends PartialType(CreateReservationDto) {
+  @IsDate()
+  @Type(() => Date)
+  startDate: Date;
+  @IsDate()
+  @Type(() => Date)
+  endDate: Date;
+  @IsString()
+  @IsNotEmpty()
+  placeId: string;
+  @IsString()
+  @IsNotEmpty()
+  invoiceId: string;
+}
