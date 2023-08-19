607e340ca3e87a5b25e984749a814cbc0906ddce
Mobin Yazdanparast <mobinyazdanparast9@gmail.com>
2023-08-19 15:39:46 +0330

    config mongo db

---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigurableModuleClass } from '@nestjs/common/cache/cache.module-definition';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get('MONGODB_URI'),
+        dbName: config.get('MONGODB_DB'),
+        user: config.get('MONGODB_USERNAME'),
+        pass: config.get('MONGODB_PASSWORD'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule extends ConfigurableModuleClass {}
